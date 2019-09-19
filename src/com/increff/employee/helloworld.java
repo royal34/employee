8f364b4eb0d7e01a026ac45840761e93360e7caf
royal34 <samarthkoul3344@gmail.com>
2019-09-19 10:27:25 +0530

    draft code

---
+++ src/com/increff/employee/helloworld.java
@@ -0,0 +1,11 @@
+package com.increff.employee;
+import java.util.Scanner;
+public class helloworld {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int n=input.nextInt();
+		for(int i=0;i<n;i++)
+			System.out.print(i);
+		//System.out.println("Hello world");
+	}
+}
