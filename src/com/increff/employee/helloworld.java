9a0d54605d1b9b2e75eb9cab0cc9b8292433ef6b
royal34 <samarthkoul3344@gmail.com>
2019-09-20 10:34:40 +0530

    c++

--- src/com/increff/employee/helloworld.java
+++ src/com/increff/employee/helloworld.java
@@ -1,11 +1,16 @@
 package com.increff.employee;
+
 import java.util.Scanner;
+
 public class helloworld {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		int n=input.nextInt();
-		for(int i=0;i<n;i++)
-			System.out.print(i);
-		//System.out.println("Hello world");
+		int n = input.nextInt();
+		int arr[] = new int[n];
+		for (int i = 0; i < n; i++)
+			arr[i] = i + 1;
+		for (int i = 0; i < n; i++)
+			System.out.print(arr[i] + " ");
+		// System.out.println("Hello world");
 	}
 }
